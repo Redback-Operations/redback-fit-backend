dd7969a90881a38e4a79b0b54d157eb175edc488
Chansada <pansuangsuang@gmail.com>
2025-08-27 10:47:25 +1000

    add username to signup page

--- auth/routes.py
+++ auth/routes.py
@@ -1,29 +1,45 @@
 from flask import Blueprint, render_template, request, redirect, url_for
 from flask_login import login_user, logout_user, login_required
-from models import db, UserCredential
+from models import db, UserCredential, UserProfile
 
 auth_bp = Blueprint('auth', __name__)
+
 
 @auth_bp.route('/signup', methods=['GET', 'POST'])
 def signup():
     error = None
     if request.method == 'POST':
+        name = request.form.get('name').strip()
         email = request.form.get('email').strip().lower()
         password = request.form.get('password')
 
         try: 
-            if not email or not password:
-                raise ValueError("Email and password are required.")
+            if not name or not email or not password:
+                raise ValueError("Name, email and password are required.")
             if UserCredential.query.filter_by(email=email).first():
                 raise ValueError("Email already registered.")   
             
+            # Create user credentials
             new_user = UserCredential(email=email)
             new_user.set_password(password)
             db.session.add(new_user)
+            db.session.flush()  # Ensure new_user.id is available
+
+            
+            profile = UserProfile(
+                user_id=new_user.id,
+                name=name,
+                account=email,
+                birthDate="---", #placeholder
+                gender="---", #placeholder
+            )
+            db.session.add(profile)
             db.session.commit()
+
 
             login_user(new_user)
             return redirect(url_for("home"))
+        
         except Exception as e:
             error = "Signup failed. "+ str(e)
 
